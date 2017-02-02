5fe0471d51fa94179046ad576992366e1f3f793c
winner <winner@winner-laptop>
2017-02-02 13:20:03 +0200

    Update structure

--- src/lesson1/HomeTasks.java
+++ src/lesson1/HomeTasks.java
@@ -1,24 +1,14 @@
 package lesson1;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
-public class HomeTask {
+import java.io.IOException;
+
+public class HomeTasks {
 	
 	public static void main(String ... args) throws IOException{
-		//Create BufferedReader instance for input from console
-		BufferedReader bufRead = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter the radius: ");
-		//Input radius from console 
-		String radiusStr = bufRead.readLine();
-		double radius = Double.parseDouble(radiusStr);
-		//Calculate the Perimeter
-		Double perimeter = radius * Math.PI;
 		
-		System.out.println("Perimeter is: " + perimeter.toString());
-		//Calculate the Square
-		Double sqr = (Math.PI*Math.sqrt(radius))/4;
-		System.out.println("Square is: " + sqr.toString());
+		//HomeTask1 task1 = new HomeTask1();
+		HomeTask3 task3 = new HomeTask3();
+		
 	}
 	
 }
