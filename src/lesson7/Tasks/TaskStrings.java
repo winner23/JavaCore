a2eeed45589d536e97f6f1bd5bb4ebf19cfa3fd3
Winner23 <v.winner23@gmail.com>
2017-02-19 17:35:36 +0200

    Second task

--- src/lesson7/Tasks/TaskStrings.java
+++ src/lesson7/Tasks/TaskStrings.java
@@ -1,9 +1,21 @@
 package lesson7.Tasks;
 
+import java.util.Scanner;
+
 public class TaskStrings {
 
+	static private Scanner scanner = new Scanner(System.in); 
+	
     private boolean checkSubstring(String string, String subString){
         return string.contains(subString);
+    }
+    
+    private String input(String msg){
+		
+    	System.out.print(msg);
+    	
+    	return scanner.next();
+    	
     }
 
     public static void main(String[] args) {
@@ -13,7 +25,12 @@ public class TaskStrings {
         String str2 = "IT";
 
         System.out.println(task.checkSubstring(str1,str2));
-
+        
+        String surname = task.input("Enter Surame: ");
+        String name = task.input("Enter Name: ");
+        String nampatronymic = task.input("Enter Patronymic Name: ");
+        
+        
 
     }
 }
